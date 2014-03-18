b358c890ae4c7b894f1b3fc4b3083ee9aaf35860
Tony Huang <eetonyhuang@gmail.com>
2014-03-17 18:18:04 -0700

    IEEE 1981,2005 EXC Parser

--- ieee.odm_pss/src/org/ieee/odm/adapter/psse/parser/dynamic/exciter/PSSEExcIEEE1981AC1Parser.java
+++ ieee.odm_pss/src/org/ieee/odm/adapter/psse/parser/dynamic/exciter/PSSEExcIEEE1981AC1Parser.java
@@ -5,27 +5,24 @@ import org.ieee.odm.adapter.psse.parser.aclf.BasePSSEDataParser;
 import org.ieee.odm.common.ODMException;
 import org.ieee.odm.model.base.ODMModelStringUtil;
 
-public class PSSEExcIEEE1968Type1Parser extends BasePSSEDataParser {
-	public PSSEExcIEEE1968Type1Parser(PsseVersion ver) {
+public class PSSEExcIEEE1981AC1Parser extends BasePSSEDataParser {
+	public PSSEExcIEEE1981AC1Parser(PsseVersion ver) {
 		super(ver);
 	}
-	
-	/*
-	 * PSSE IEEET1 -> IEEE 1968 Type 1
-	 * IBUS, 扞EEET1� I, TR, KA, TA, VRMAX, VRMIN, KE, TE, KF, TF, 0., E1, SE(E1), E2, SE(E2)/
-	 */
+    
+	// PSS/E EXAC1 type
 	
 	@Override
 	public String[] getMetadata() {
 		return new String[]{
 			//  0----------1----------2----------3----------4
-				"IBUS", "Type",   "MachId",   "TR",       "KA",
+				"IBUS", "Type",   "MachId",   "TR",       "TB",
 			//  5----------6----------7----------8----------9
-				"TA",    "VRMAX",   "VRMIN",   "KE",      "TE",  	 
+				"TC",    "KA",      "TA",    "VRMAX",   "VRMIN",    	 
 			//  10----------11----------12--------13--------14	
-				"KF",    "TF",      "Switch",   "E1",   "SE(E1)",		   
-			//  15----------16----
-				"E2",    "SE(E2)"			
+				"TE",      "KF",      "TF",      "KC",     "KD", 
+			//  15----------16----------17--------18--------19	
+				"KE",      "E1",      "SE(E1)",	 "E2",    "SE(E2)"			
 		};
 		
 	}
@@ -36,7 +33,7 @@ public class PSSEExcIEEE1968Type1Parser extends BasePSSEDataParser {
 		String[] strAry=lineStr.split("\\s+");
 		int cnt =strAry.length;
 		for (int i = 0; i <cnt ; i++){
-			if(i==1){// Model type, need to trim the quote
+			if(i==1){//Model type, need to trim the quote
 				setValue(i,ODMModelStringUtil.trimQuote(strAry[i].trim()));
 			}
 			else setValue(i, strAry[i].trim());
@@ -44,6 +41,4 @@ public class PSSEExcIEEE1968Type1Parser extends BasePSSEDataParser {
 		
 	}
 	
-	
-
 }
--- ieee.odm_pss/src/org/ieee/odm/adapter/psse/parser/dynamic/exciter/PSSEExcIEEE2005ST3AParser.java
+++ ieee.odm_pss/src/org/ieee/odm/adapter/psse/parser/dynamic/exciter/PSSEExcIEEE2005ST3AParser.java
@@ -5,27 +5,29 @@ import org.ieee.odm.adapter.psse.parser.aclf.BasePSSEDataParser;
 import org.ieee.odm.common.ODMException;
 import org.ieee.odm.model.base.ODMModelStringUtil;
 
-public class PSSEExcIEEE1968Type1Parser extends BasePSSEDataParser {
-	public PSSEExcIEEE1968Type1Parser(PsseVersion ver) {
+public class PSSEExcIEEE2005ST3AParser extends BasePSSEDataParser {
+	public PSSEExcIEEE2005ST3AParser(PsseVersion ver) {
 		super(ver);
 	}
 	
 	/*
-	 * PSSE IEEET1 -> IEEE 1968 Type 1
-	 * IBUS, 扞EEET1� I, TR, KA, TA, VRMAX, VRMIN, KE, TE, KF, TF, 0., E1, SE(E1), E2, SE(E2)/
+	 * PSS/E ESST3A type -> IEEE 1992/2005 Type ST3A
+	 * IBUS, 'ESST3A', TR, VIMAX,VIMIN, KM,TC,TB, KA, TA, VRMAX, VRMIN, KG, KP, KI, VBMAX, KC, XL,VGMAX, THETAP, TM, VMMAX, VMMIN/
 	 */
 	
 	@Override
 	public String[] getMetadata() {
 		return new String[]{
 			//  0----------1----------2----------3----------4
-				"IBUS", "Type",   "MachId",   "TR",       "KA",
+				"IBUS",  "Type",    "MachId",   "TR",       "VIMAX",
 			//  5----------6----------7----------8----------9
-				"TA",    "VRMAX",   "VRMIN",   "KE",      "TE",  	 
-			//  10----------11----------12--------13--------14	
-				"KF",    "TF",      "Switch",   "E1",   "SE(E1)",		   
-			//  15----------16----
-				"E2",    "SE(E2)"			
+				"VIMIN",    "KM",    "TC",      "TB",      "KA",    		     	 
+			//  10----------11----------12--------13--------14
+				"TA",	 "VRMAX",   "VRMIN",     "KG",     "KP",
+			//  15----------16----------17--------18--------19	
+				"KI",    "VBMAX",    "KC",     	 "XL",    "VGMAX",
+			//  20----------21----------22--------23--------24
+				"THETAP",   "TM",    "VMMAX",   "VMMIN"
 		};
 		
 	}
@@ -36,14 +38,12 @@ public class PSSEExcIEEE1968Type1Parser extends BasePSSEDataParser {
 		String[] strAry=lineStr.split("\\s+");
 		int cnt =strAry.length;
 		for (int i = 0; i <cnt ; i++){
-			if(i==1){// Model type, need to trim the quote
+			if(i==1){//Model type, need to trim the quote
 				setValue(i,ODMModelStringUtil.trimQuote(strAry[i].trim()));
 			}
 			else setValue(i, strAry[i].trim());
 		}
 		
 	}
-	
-	
 
 }
